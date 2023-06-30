3501493ecca31f076621bf47a16a942be4d25cee
Aymen Hammami <hammami.aym@outlook.com>
2023-06-30 12:20:56 +0100

    added config and source files

---
+++ Source/ProjectPersia/ProjectPersia.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ProjectPersia.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectPersia, "ProjectPersia" );
